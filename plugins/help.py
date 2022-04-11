3aef54f7bba81246fff17881fcdb4f8867518b72
YungDev92 <102784611+YungDev92@users.noreply.github.com>
2022-04-11 09:53:03 -0400

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,5 +4,5 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Bot Can Download Mp3/Mp4 from yotubebe Powered by @BeastX_Bots"
+    helptxt = f"Bot Can Download Mp3/Mp4 from youtube Powered by @yung92"
     await message.reply_text(helptxt)
